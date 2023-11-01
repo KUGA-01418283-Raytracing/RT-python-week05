9211fdb6e130cba7c1f0cb3a88f8b0e323ae5444
Tanaboon <darkhikaru@gmail.com>
2023-11-01 12:50:36 +0700

    Catching up week04
    update solutions to catch up codes for week4-5

--- RT_integrator.py
+++ RT_integrator.py
@@ -26,11 +26,3 @@ class Integrator():
         a = (unit_direction.y() + 1.0)*0.5
         return rtu.Color(1,1,1)*(1.0-a) + rtu.Color(0.5, 0.7, 1.0)*a
 
-    # def get_color(self, rGen_ray, scene):
-
-    #     found_hit = scene.find_intersection(rGen_ray, rtu.Interval(0.000001, rtu.infinity_number))
-    #     if found_hit == True:
-    #         tmpN = scene.getHitList().getNormal()
-    #         return (rtu.Color(tmpN.x(), tmpN.y(), tmpN.z()) + rtu.Color(1,1,1))*0.5
-
-    #     return self.background_color(rGen_ray)
--- RT_object.py
+++ RT_object.py
@@ -22,14 +22,23 @@ class Sphere(Object):
     def printInfo(self):
         self.center.printout()        
     
-    # Assignment 1
-    def intersect(self, rRay, cInterval):
-        
-        # find roots of the quadratic solution.
+    def intersect(self, rRay, cInterval):        
+        oc = rRay.getOrigin() - self.center
+        a = rRay.getDirection().len_squared()
+        half_b = rtu.Vec3.dot_product(oc, rRay.getDirection())
+        c = oc.len_squared() - self.radius*self.radius
+        discriminant = half_b*half_b - a*c 
 
-        # check if the positive root is in the interval
+        if discriminant < 0:
+            return None
+        sqrt_disc = math.sqrt(discriminant)
+
+        root = (-half_b - sqrt_disc) / a 
+        if not cInterval.surrounds(root):
+            root = (-half_b + sqrt_disc) / a 
+            if not cInterval.surrounds(root):
+                return None
             
-        # generate and return a hit info
         hit_t = root
         hit_point = rRay.at(root)
         hit_normal = (hit_point - self.center) / self.radius
@@ -45,35 +54,41 @@ class Quad(Object):
         self.Uvec = vU
         self.Vvec = vV
         self.material = mMat
-
-        # Assignment 2
-        # calculating quad parameters 
-        self.uxv = None
-        self.normal = None 
-        self.D = None
-        self.Wvec = None
+        self.uxv = rtu.Vec3.cross_product(self.Uvec, self.Vvec)
+        self.normal = rtu.Vec3.unit_vector(self.uxv)
+        self.D = rtu.Vec3.dot_product(self.normal, self.Qpoint)
+        self.Wvec = self.uxv / rtu.Vec3.dot_product(self.uxv, self.uxv)
 
     def add_material(self, mMat):
         self.material = mMat
 
     # Assignment 3
     def intersect(self, rRay, cInterval):
+        denom = rtu.Vec3.dot_product(self.normal, rRay.getDirection())
+        # if parallel
+        if rtu.Interval.near_zero(denom):
+            return None
+
+        # if it is hit.
+        t = (self.D - rtu.Vec3.dot_product(self.normal, rRay.getOrigin())) / denom
+        if not cInterval.contains(t):
+            return None
         
-        # if the ray is parallel to the plane
-
-        # if the ray hits the plane.
-        
-
-        # determine if the intersection point lies on the quad's plane.
-
-        # generate and return a hit info
         hit_t = t
         hit_point = rRay.at(t)
         hit_normal = self.normal
+
+        # determine if the intersection point lies on the quad's plane.
+        planar_hit = hit_point - self.Qpoint
+        alpha = rtu.Vec3.dot_product(self.Wvec, rtu.Vec3.cross_product(planar_hit, self.Vvec))
+        beta = rtu.Vec3.dot_product(self.Wvec, rtu.Vec3.cross_product(self.Uvec, planar_hit))
+        if self.is_interior(alpha, beta) is None:
+            return None
+
         hinfo = rtu.Hitinfo(hit_point, hit_normal, hit_t, self.material)
         hinfo.set_face_normal(rRay, hit_normal)
         return hinfo
-
+    
     def is_interior(self, fa, fb):
         delta = 0   
         if (fa<delta) or (1.0<fa) or (fb<delta) or (1.0<fb):
